0461275963a452f496956bf11182e34dc67c6431
martin <martin.tursk@hotmail.com>
2025-03-19 18:18:52 +0200

    kontrolltöö tehtud

--- pythonkontrolltoo.py
+++ pythonkontrolltoo.py
@@ -4,55 +4,48 @@ import requests
 url = 'https://metshein.com/kordamine/json/kasutajad.json'
 
 response = requests.get(url)
-
-#################################################################################
-
 if response.status_code == 200:
     print('Kasutajad on olemas')
     info = response.json()
     kasutajad =  info["kasutajad"]
-    admin_kasutajad = [user["nimi"].replace('.' , '') for user in kasutajad if "admin" in user["roll"]]
+#################################################################################
+  
+# esimene harjutus
+
+    admin_kasutajad = [kasutaja["nimi"].replace('.' , '') for kasutaja in kasutajad if "admin" in kasutaja["roll"]]
     print("Admin kasutajad: ")
     for admin in admin_kasutajad:
         print(admin)
-else:
-    print('Kasutajad ei leitud')
 
 #################################################################################
 
-admin_arv = 0
-for user in info["kasutajad"]:
-    if "admin" in user["roll"]:
-        admin_arv += 1
-    print(f'Administraatoreid on {admin_arv}')
+# teine harjutus
 
-else:
-    print('Kasutajad ei leitud')
+    # for kasutaja in info["kasutajad"]: siin laks mul pekki
+
+    admin_arv = 0
+    for kasutaja in kasutajad:
+        if "admin" in kasutaja["roll"]:
+            admin_arv = admin_arv + 1
+    print("Administraatoreid on " + str(admin_arv))
+  
+
 #################################################################################
+ 
+# kolmas harjutus
 
-peatatud_kasutajad = [
-    user["nimi"]
-    for user in info["kasutajad"]
-    if user["staatus"] == "peatatud"
-]
+    peatatud_kasutajad = []
+    for kasutaja in kasutajad:
+        if kasutaja["staatus"] == "peatatud":
+            peatatud_kasutajad.append(kasutaja["nimi"])
 
-print("Peatatud kasutajad:")
-if peatatud_kasutajad:
-    for nimi in peatatud_kasutajad:
-        print(nimi)
+    print("Peatatud kasutajad:")
+    if peatatud_kasutajad:
+        for nimi in peatatud_kasutajad:
+            print(nimi)
 else:
     print("Kasutajaid ei leitud")
 
+#################################################################################
 
-
-
-
-
-
-
-
-
-
-
-
-
+# kontrolltöö tehtud
